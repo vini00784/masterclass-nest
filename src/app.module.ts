e4a50a308256e9a3908082e0d498522139513f54
vini00784 <vo70346@gmail.com>
2024-04-23 09:26:15 -0300

    feat: import app providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
+import { RocketMembersRepository } from './repositories/rocket-members-repository';
+import { PrismaRocketMembersRepository } from './repositories/prisma/prisma-rocket-members-repository';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    PrismaService, 
+    {
+      provide: RocketMembersRepository,
+      useClass: PrismaRocketMembersRepository
+    }
+  ],
 })
 export class AppModule {}
