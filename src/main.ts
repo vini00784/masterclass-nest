45c3c2697a393f03ffd47aeffec5e5494d9d50df
vini00784 <vo70346@gmail.com>
2024-04-23 09:32:22 -0300

    feat: add global validator to classes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe()); // O Nest agora valida automaticamente os bodys a partir das classes que são passadas para eles. Nas classes, usar decorators para as devidas validações
+
   await app.listen(3000);
 }
 bootstrap();
